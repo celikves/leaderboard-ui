4f8ecfbd34e66c20788bd7a0769c34ff58d4f877
Vesile Çelik <73591684+celikves@users.noreply.github.com>
2024-10-23 13:24:02 +0300

    scheduler trigger buttons added

--- game-leaderboard-frontend/src/Explanation.jsx
+++ game-leaderboard-frontend/src/Explanation.jsx
@@ -1,17 +1,67 @@
-import React from 'react';
-
 const Explanation = () => {
+  const handlePostRequest = async (url) => {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      alert("Request sent successfully!");
+    } catch (error) {
+      console.error("Error sending request:", error);
+      alert("Failed to send request.");
+    }
+  };
   return (
     <div className="bg-gray text-white p-4 rounded-md my-4 text-sm m-4">
-      <h2 className="text-lg font-semibold mb-2 text-main">Leaderboard Information</h2>
-      <p>This table displays the top players ranked by their weekly earnings.</p>
+      <h2 className="text-lg font-semibold mb-2 text-main">
+        Leaderboard Information
+      </h2>
+      <p>
+        This table displays the top players ranked by their weekly earnings.
+      </p>
       <ul className="list-disc list-inside">
-        <li><strong>Country:</strong> Player's country of origin.</li>
-        <li><strong>Username:</strong> The player's unique username.</li>
-        <li><strong>Rank:</strong> The player's current rank based on their earnings.</li>
-        <li><strong>Money:</strong> Total earnings by the player this week.</li>
-        <li><strong>Daily Diff:</strong> Changes in ranking today. ("Green" for positive, "Red" for negative and "Yellow" for zero.)</li>
+        <li>
+          <strong>Rank:</strong> The player's current rank based on their
+          earnings.
+        </li>
+        <li>
+          <strong>Username:</strong> The player's unique username.
+        </li>
+        <li>
+          <strong>Money:</strong> Total earnings by the player this week.
+        </li>
+        <li>
+          <strong>Country:</strong> Player's country of origin.
+        </li>
+        <li>
+          <strong>Daily Diff:</strong> Changes in ranking today. ("Green" for
+          positive, "Red" for negative and "Yellow" for zero.)
+        </li>
       </ul>
+      <div className="mt-4">
+        <button
+          onClick={() =>
+            handlePostRequest(
+              "http://localhost:3005/api/leaderboard/calculateYesterdayRanks"
+            )
+          }
+          className="schedule-button"
+        >
+          Calculate Yesterday Ranks
+        </button>
+        <button
+          onClick={() =>
+            handlePostRequest(
+              "http://localhost:3005/api/leaderboard/weeklyPrizeDistribution"
+            )
+          }
+          className="schedule-button"
+        >
+          Weekly Prize Distribution
+        </button>
+      </div>
     </div>
   );
 };
--- game-leaderboard-frontend/src/Leaderboard.jsx
+++ game-leaderboard-frontend/src/Leaderboard.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css';
-import 'ag-grid-community/styles/ag-theme-alpine.css';
-import axios from 'axios';
-import './App.css';
+import { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import axios from "axios";
+import "./App.css";
 
 const Leaderboard = () => {
   const [rowData, setRowData] = useState([]);
@@ -12,18 +12,20 @@ const Leaderboard = () => {
   useEffect(() => {
     const fetchPlayers = async () => {
       try {
-        const response = await axios.get('http://localhost:3005/api/leaderboard');
-        const playersData = response.data.map((player) => ({
+        const response = await axios.get(
+          "http://localhost:3005/api/leaderboard/6718bcd1fca6ec0d8cff67a3"
+        );
+        const playersData = response.data.leaderboard.map((player) => ({
           country: player.country,
           username: player.name,
-          rank: player.rank || 'N/A',
-          money: player.totalEarnings || 0,
-          dailyDiff: player.dailyDiff || 0 
-        }));       
+          rank: player.rank || "N/A",
+          money: player.earnings || 0,
+          dailyDiff: player.dailyDiff || 0,
+        }));
         setRowData(playersData);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching players:', error);
+        console.error("Error fetching players:", error);
         setLoading(false);
       }
     };
@@ -32,51 +34,51 @@ const Leaderboard = () => {
   }, []);
 
   const columnDefs = [
-    { headerName: 'Country', field: 'country', flex: 1, minWidth: 70 },
-    { headerName: 'Username', field: 'username', flex: 1, minWidth: 70 },
-    { headerName: 'Rank', field: 'rank', flex: 1, minWidth: 70 },
-    { headerName: 'Money', field: 'money', flex: 1, minWidth: 70 },
+    { headerName: "Rank", field: "rank", flex: 1, minWidth: 40 },
+    { headerName: "Username", field: "username", flex: 1, minWidth: 70 },
+    { headerName: "Money", field: "money", flex: 1, minWidth: 70 },
+    { headerName: "Country", field: "country", flex: 1, minWidth: 70 },
     {
-      headerName: 'Daily Diff',
-      field: 'dailyDiff',
+      headerName: "Daily Diff",
+      field: "dailyDiff",
       flex: 1,
       minWidth: 70,
-      cellStyle: params => {
+      cellStyle: (params) => {
         if (params.value > 0) {
-          return { color: 'green', fontWeight: 'bold' };
+          return { color: "green", fontWeight: "bold" };
         } else if (params.value === 0) {
-          return { color: 'yellow', fontWeight: 'bold' };
+          return { color: "yellow", fontWeight: "bold" };
         } else {
-          return { color: 'red', fontWeight: 'bold' };
+          return { color: "red", fontWeight: "bold" };
         }
       },
     },
   ];
 
   const localeText = {
-    of: '-',
-    to: '/',
-    more: 'more',
-    less: 'less',
-    paginationPage: '',
-    paginationOf: 'of',
-    paginationTo: 'to',
-    paginationFirstPage: 'First',
-    paginationLastPage: 'Last',
-    paginationNextPage: 'Next',
-    paginationPreviousPage: 'Previous',
-    paginationShowing: 'Showing',
-    paginationBetween: 'between',
-    paginationFooterRowCount: 'Total rows',
-    rangeSeparator: 'to',
-    loadingOoo: 'Loading...',
+    of: "-",
+    to: "/",
+    more: "more",
+    less: "less",
+    paginationPage: "",
+    paginationOf: "of",
+    paginationTo: "to",
+    paginationFirstPage: "First",
+    paginationLastPage: "Last",
+    paginationNextPage: "Next",
+    paginationPreviousPage: "Previous",
+    paginationShowing: "Showing",
+    paginationBetween: "between",
+    paginationFooterRowCount: "Total rows",
+    rangeSeparator: "to",
+    loadingOoo: "Loading...",
   };
 
   return (
     <div className="w-full p-4 bg-gray-950">
       <div
         className="ag-theme-alpine-dark"
-        style={{ width: '100%', minWidth: '350px' }}
+        style={{ width: "100%", minWidth: "350px" }}
       >
         {loading ? (
           <div className="flex items-center justify-center h-screen text-white text-2xl">
@@ -94,7 +96,7 @@ const Leaderboard = () => {
             pagination={true}
             paginationPageSize={20}
             paginationAutoPageSize={false}
-            paginationPageSizeOptions={[20, 50, 100]} 
+            paginationPageSizeOptions={[20, 50, 100]}
             localeText={localeText}
           />
         )}
